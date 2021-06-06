fa21ed91ee8dc119cb2992d8c834d992baf72f9c
Phinyo <phinyo.it@gmail.com>
2021-06-06 22:49:39 +0700

    update

---
+++ single-product.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"><!--<![endif]-->
+    <head>
+        <!-- Basic Page Needs -->
+        <meta charset="UTF-8">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <title>ข้อมูลสินค้า | ตลาดอิเล็กทรอนิกส์ปลาแห่งสยาม</title>
+
+        <meta name="author" content="Fishsiam">
+        <meta name="keywords" content="แพลตฟอร์ม,ตลาดอิเล็กทรอนิกส์,ปลาแห่งสยาม,สยาม,ปลาสวยงาม , ปลากัด, ปลาไทย">
+        <meta name="description" content="แพลตฟอร์ม,ตลาดอิเล็กทรอนิกส์,ปลาแห่งสยาม,สยาม,ปลาสวยงาม , ปลากัด, ปลาไทย">
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <!-- Boostrap style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
+
+        <!-- Theme style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+
+        <!-- Reponsive -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
+
+        <link rel="shortcut icon" href="favicon/favicon.png">
+         <link href="stylesheets/dashboard.css" rel="stylesheet" />
+
+
+    </head>
+ 
+
+    <body class="header_sticky">
+        <div class="boxed">
+ 
+            <div class="overlay"></div>
+
+            <!-- Preloader -->
+            <div class="preloader">
+                <div class="clear-loading loading-effect-2">
+                    <span></span>
+                </div>
+            </div><!-- /.preloader -->
+
+            
+             <?php include 'inc/header-top.php'; ?>
+
+
+
+
+           
+
+<section class="flat-breadcrumb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="breadcrumbs">
+                            <li class="trail-item">
+                                <a href="#" title="">หน้าหลัก</a>
+                                <span><img src="images/icons/arrow-right.png" alt=""></span>
+                            </li>
+
+                            <li class="trail-item">
+                                <a href="#" title="">ร้านรักษ์ปลา</a>
+                                <span><img src="images/icons/arrow-right.png" alt=""></span>
+                            </li>
+
+                           
+                           
+                            <li class="trail-end">
+                                <a href="#" title="">ปลาสีส้ม ตัวเล็ก</a>
+                            </li>
+
+                        </ul><!-- /.breacrumbs -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-breadcrumb -->
+
+
+
+
+            <section class="flat-product-detail">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="flexslider">
+                            <ul class="slides">
+                                <li data-thumb="images/product/flexslider/thumb/2.jpg">
+                                  <a href='#' id="zoom" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
+                                  <span>ขาย</span>
+                                </li>
+                                <li data-thumb="images/product/flexslider/thumb/3.jpg">
+                                  <a href='#' id="zoom1" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
+                                </li>
+                                <li data-thumb="images/product/flexslider/thumb/4.jpg">
+                                  <a href='#' id="zoom2" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
+                                  <span>NEW</span>
+                                </li>
+                                <li data-thumb="images/product/flexslider/thumb/5.jpg">
+                                  <a href='#' id="zoom3" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
+                                </li>
+                                <li data-thumb="images/product/flexslider/thumb/6.jpg">
+                                  <a href='#' id="zoom4" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
+                                </li>
+                            </ul><!-- /.slides -->
+                        </div><!-- /.flexslider -->
+                    </div><!-- /.col-md-6 -->
+                    <div class="col-md-6">
+                        <div class="product-detail">
+                            <div class="header-detail">
+                                <h4 class="name">ปลาสีส้ม ตัวเล็ก</h4>
+                                <!-- <div class="category">
+                                    หมวดหมู่ : ปลาทอง
+                                </div> -->
+                                <div class="product-id">
+                                   หมวดหมู่ : <span class="id">ปลาทอง</span>
+                                </div>
+                                <div class="reviewed">
+                                    <div class="review">
+                                        <div class="queue">
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="text">
+                                            <span>4 (การให้คะแนนทั้งหมด 4 คน)</span>
+                                        </div>
+                                    </div><!-- /.review -->
+                                    <div class="status-product">
+                                        สถานะสินค้า <span>มีสินค้า</span>
+                                    </div>
+                                </div><!-- /.reviewed -->
+                            </div><!-- /.header-detail -->
+                            <div class="content-detail">
+                                <div class="price">
+                                    <div class="regular">
+                                        ราคาปกติ ฿30
+
+                                    </div>
+
+                                    <div class="sale">
+                                      <font color="#000000">ราคา</font> ฿15
+
+                                    </div>
+                                </div>
+                                <div class="info-text">
+                                  รายละเอียดย่อ : เป็นปลาตัวเล็ก สีส้ม
+                                </div>
+                                
+                            </div><!-- /.content-detail -->
+                            <div class="footer-detail">
+                               
+                                <div class="box-cart style2">
+                                    <div class="btn-add-cart">
+                                        <a href="#" title=""><img src="images/icons/add-cart.png" alt="">เพิ่มไปยังรถเข็น</a>
+                                    </div>
+                                   
+                                </div><!-- /.box-cart -->
+                                <div class="social-single">
+                                    <span>SHARE</span>
+                                    <ul class="social-list style2">
+                                        <li>
+                                            <a href="#" title="">
+                                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" title="">
+                                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" title="">
+                                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" title="">
+                                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" title="">
+                                                <i class="fa fa-dribbble" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" title="">
+                                                <i class="fa fa-google" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                    </ul><!-- /.social-list -->
+                                </div><!-- /.social-single -->
+                            </div><!-- /.footer-detail -->
+                        </div><!-- /.product-detail -->
+                    </div><!-- /.col-md-6 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-product-detail -->
+
+            
+
+
+     
+
+       
+
+
+    
+
+
+    <?php include 'inc/footer.php'; ?>
+        
+
+        </div><!-- /.boxed -->
+
+        <!-- Javascript -->
+        <script type="text/javascript" src="javascript/jquery.min.js"></script>
+        <script type="text/javascript" src="javascript/tether.min.js"></script>
+        <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+        <script type="text/javascript" src="javascript/waypoints.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.circlechart.js"></script>
+        <script type="text/javascript" src="javascript/easing.js"></script>
+<script type="text/javascript" src="javascript/jquery.zoom.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+        <script type="text/javascript" src="javascript/owl.carousel.js"></script>
+        
+        <script type="text/javascript" src="javascript/jquery-ui.js"></script>
+        <script type="text/javascript" src="javascript/jquery.mCustomScrollbar.js"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtRmXKclfDp20TvfQnpgXSDPjut14x5wk&region=GB"></script>
+        <script type="text/javascript" src="javascript/gmap3.min.js"></script>
+        <script type="text/javascript" src="javascript/waves.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.countdown.js"></script>
+
+        <script type="text/javascript" src="javascript/main.js"></script>
+
+    </body> 
+</html>
+
