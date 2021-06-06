d15adbe19134599ff37179900fee106519a0e91b
Phinyo <phinyo.it@gmail.com>
2021-06-06 23:53:53 +0700

    update

--- single-product.php
+++ single-product.php
@@ -22,7 +22,6 @@
         <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
 
         <link rel="shortcut icon" href="favicon/favicon.png">
-         <link href="stylesheets/dashboard.css" rel="stylesheet" />
 
 
     </head>
@@ -84,23 +83,19 @@
                     <div class="col-md-6">
                         <div class="flexslider">
                             <ul class="slides">
-                                <li data-thumb="images/product/flexslider/thumb/2.jpg">
+                                <li data-thumb="images/product/flexslider/big-size-1.jpg">
                                   <a href='#' id="zoom" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
                                   <span>ขาย</span>
                                 </li>
-                                <li data-thumb="images/product/flexslider/thumb/3.jpg">
+                                <li data-thumb="images/product/flexslider/big-size-1.jpg">
                                   <a href='#' id="zoom1" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
                                 </li>
-                                <li data-thumb="images/product/flexslider/thumb/4.jpg">
+                                <li data-thumb="images/product/flexslider/big-size-1.jpg">
                                   <a href='#' id="zoom2" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
                                   <span>NEW</span>
                                 </li>
-                                <li data-thumb="images/product/flexslider/thumb/5.jpg">
-                                  <a href='#' id="zoom3" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
-                                </li>
-                                <li data-thumb="images/product/flexslider/thumb/6.jpg">
-                                  <a href='#' id="zoom4" class='zoom'><img src="images/product/flexslider/big-size-1.jpg" alt='' width='400' height='300' /></a>
-                                </li>
+                               
+                              
                             </ul><!-- /.slides -->
                         </div><!-- /.flexslider -->
                     </div><!-- /.col-md-6 -->
@@ -198,6 +193,336 @@
                 </div><!-- /.row -->
             </div><!-- /.container -->
         </section><!-- /.flat-product-detail -->
+
+
+
+        <section class="flat-term-conditions">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="term-conditions">
+                            <div class="text-wrap">
+
+                                <div class="card">
+                                    <div class="card-body">
+                                <h3>ปลาส้ม ตัวเล็ก</h3>
+                                 <h4>รายละเอียด</h4>
+                                <p>
+                                   ปลาทองมีถิ่นกำเนิดในประเทศจีนและญี่ปุ่น ต่อมาถูกนำไปเลี้ยงในยุโรป เมื่อศตวรรษที่ 17 และถูกนำไปเผยแพร่ในอเมริกา ในศตวรรษที่ 19 ชาวจีน และชาวญี่ปุ่นรู้จักผสมพันธ์ปลาทองมานานแล้ว และได้ปลาทองลูกผสม
+                                </p><br>
+                                <p>
+                                    <a href="#" class="btn btn-outline-success" >ของยังมีอยู่ไหม</a> 
+                                    <a href="#" class="btn btn-outline-success" >ลดได้ไหม ?</a> 
+                                    <a href="#" class="btn btn-outline-success" >ราคานี้รวมส่งไหม</a> 
+                                </p>
+                                </div>
+                                </div>
+                            </div><!-- /.text-wrap -->
+                            
+                        </div><!-- /.term-conditions -->
+                    </div><!-- /.col-md-6 -->
+
+
+                    <div class="col-md-5">
+                        <div class="term-conditions">
+                            <div class="text-wrap">
+                              
+
+
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="media mt-0">
+                                            <figure class="rounded-circle align-self-start mb-0">
+
+                                                <span class="avatar avatar-xxl" style="background-image: url(images/slider/logo-Shop03.jpg)"></span>
+
+                                            </figure>
+
+                                            <div class="media-body">
+                                                 
+                                                <h3 class="time-title p-0 mb-0 font-weight-semibold leading-normal">
+                                                คุณสมหมาย รักปลา</h3>
+                                                ร้าน FH ชอบปลา <br>
+                                                มีนบุรี กรุงเทพมหานคร
+                                                  <div class="reviewed">
+                                    <div class="review">
+                                        <div class="queue">
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                            <i class="fa fa-star" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="text">
+                                            <span>4 (การให้คะแนนทั้งหมด 4 คน)</span>
+                                        </div>
+                                    </div><!-- /.review -->
+                                 
+                                </div><!-- /.reviewed -->
+                                            </div>
+
+
+                                           
+                                        </div>
+                                    </div>
+                                    <div class="card-footer border-top">
+                                       
+                                        <button type="button" class="btn btn-rss mb-1 fa fa-comments">  พูดคุยกับผู้ขาย</button>
+                                        <button type="button" class="btn btn-rss mb-1 fa fa-phone"> ดูเบอร์ผู้ขาย</button>
+                                        <br>
+                                        <font color="#32cd32" > <h5 align = 'right'>ร้านนี้ผ่านมาตรฐาน GAP</h5> </font>
+
+                                    </div>
+                                </div>
+                           
+
+                               
+                            </div><!-- /.text-wrap -->
+                            
+                        </div><!-- /.term-conditions -->
+                    </div><!-- /.col-md-6 -->
+
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-term-conditions -->
+
+
+
+
+
+
+
+
+
+
+
+        <section class="flat-imagebox style4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="flat-row-title">
+                            <h3>รายการสินค้า</h3>
+                        </div>
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="owl-carousel-3">
+                            
+
+                            
+                        
+                            <div class="imagebox style4">
+                                <div class="box-image">
+                                    <span class="item-new1">ขาย</span>
+                                    <a href="#" title="">
+                                        <img src="images/slider/bg-product01.jpg" alt="">
+
+                                    </a>
+
+                                </div><!-- /.box-image -->
+                                <div class="box-content">
+                                    
+                                    <div class="product-name">
+                                        <p>รายละเอียด</p>
+                                    <a href="#" title="" > <h4>ปลาการ์ตูนส้มขาว</h4></a>
+                                        <p>หมวดหมู่ : ปลาทอง</p>
+                                        <p>ร้าน : ชอบปลาดอทคอม</p>
+                                        <p>บางกอกน้อย กรุงเทพมหานคร</p>
+                                        <div class="price">
+                                                <span type="body-text" class="sale">฿</span><span class="sale">100 บาท</span>
+                                            </div>
+
+                                    </div>
+                                    <br>
+                            
+                <center><button class="button-chat"> <img src="images/icons/chat_white.png" alt="" width="20">  แชท</button>
+                <button class="button-call"> <img src="images/icons/phone_white.png" alt="" width="20"> โทร</button>
+
+                </center>
+
+
+                </div><!-- /.box-content -->
+                </div><!-- /.imagebox style4 -->
+
+
+                <div class="imagebox style4">
+                                <div class="box-image">
+                                    <span class="item-new1">ขาย</span>
+                                    <a href="#" title="">
+                                        <img src="images/slider/bg-product02.jpg" alt="">
+
+                                    </a>
+
+                                </div><!-- /.box-image -->
+                                <div class="box-content">
+                                    
+                                    <div class="product-name">
+                                        <p>รายละเอียด</p>
+                                    <a href="#" title="" > <h4>ปลาคาร์ฟ</h4></a>
+                                        
+                                        <p>หมวดหมู่ : ปลาครา์ฟ</p>
+                                        <p>ร้าน : ลุงชัยเลี้ยงปลา</p>
+                                        <p>ลาดกระบัง กรุงเทพมหานคร</p>
+                                        <div class="price">
+                                                <span type="body-text" class="sale">฿</span><span class="sale">100 บาท</span>
+                                            </div>
+
+                                    </div>
+                                    <br>
+                            
+                <center><button class="button-chat"> <img src="images/icons/chat_white.png" alt="" width="20">  แชท</button>
+                <button class="button-call"> <img src="images/icons/phone_white.png" alt="" width="20"> โทร</button>
+
+
+                </center>
+                </div><!-- /.box-content -->
+                </div><!-- /.imagebox style4 -->
+
+                <div class="imagebox style4">
+                                <div class="box-image">
+                                    <span class="item-new1">ขาย</span>
+                                    <a href="#" title="">
+                                        <img src="images/slider/bg-product03.jpg" alt="">
+
+                                    </a>
+
+                                </div><!-- /.box-image -->
+                                <div class="box-content">
+                                    
+                                    <div class="product-name">
+                                        <p>รายละเอียด</p>
+                                    <a href="#" title="" > <h4>ปลาทอง</h4></a>
+                                        
+                                        <p>หมวดหมู่ : ปลาทอง</p>
+                                        <p>ร้าน : ลุงชัยเลี้ยงปลา</p>
+                                        <p>ลาดกระบัง กรุงเทพมหานคร</p>
+                                        <div class="price">
+                                                <span type="body-text" class="sale">฿</span><span class="sale">100 บาท</span>
+                                            </div>
+
+                                    </div>
+                                    <br>
+                            
+                <center><button class="button-chat"> <img src="images/icons/chat_white.png" alt="" width="20">  แชท</button>
+                <button class="button-call"> <img src="images/icons/phone_white.png" alt="" width="20"> โทร</button>
+
+                </center>
+                </div><!-- /.box-content -->
+                </div><!-- /.imagebox style4 -->
+
+                <div class="imagebox style4">
+                                <div class="box-image">
+                                    <span class="item-new1">ขาย</span>
+                                    <a href="#" title="">
+                                        <img src="images/slider/bg-product04.jpg" alt="">
+
+                                    </a>
+
+                                </div><!-- /.box-image -->
+                                <div class="box-content">
+                                    
+                                    <div class="product-name">
+                                        <p>รายละเอียด</p>
+                                    <a href="#" title="" > <h4>ปลาทอง</h4></a>
+                                        
+                                        <p>หมวดหมู่ : ปลาทอง</p>
+                                        <p>ร้าน : ลุงชัยเลี้ยงปลา</p>
+                                        <p>ลาดกระบัง กรุงเทพมหานคร</p>
+                                        <div class="price">
+                                                <span type="body-text" class="sale">฿</span><span class="sale">100 บาท</span>
+                                            </div>
+
+                                    </div>
+                                    <br>
+                            
+                <center><button class="button-chat"> <img src="images/icons/chat_white.png" alt="" width="20">  แชท</button>
+                <button class="button-call"> <img src="images/icons/phone_white.png" alt="" width="20"> โทร</button>
+
+                </center>
+                </div><!-- /.box-content -->
+                </div><!-- /.imagebox style4 -->
+
+                <div class="imagebox style4">
+                                <div class="box-image">
+                                    <span class="item-new1">ขาย</span>
+                                    <a href="#" title="">
+                                        <img src="images/slider/bg-product05.jpg" alt="">
+
+                                    </a>
+
+                                </div><!-- /.box-image -->
+                                <div class="box-content">
+                                    
+                                    <div class="product-name">
+                                        <p>รายละเอียด</p>
+                                    <a href="#" title="" > <h4>ปลาทอง</h4></a>
+                                        <p>หมวดหมู่ : ปลาทอง</p>
+                                        <p>ร้าน : ลุงชัยเลี้ยงปลา</p>
+                                        <p>ลาดกระบัง กรุงเทพมหานคร</p>
+                                        <div class="price">
+                                                <span type="body-text" class="sale">฿</span><span class="sale">100 บาท</span>
+                                            </div>
+
+                                    </div>
+                                    <br>
+                            
+                <center><button class="button-chat"> <img src="images/icons/chat_white.png" alt="" width="20">  แชท</button>
+                <button class="button-call"> <img src="images/icons/phone_white.png" alt="" width="20"> โทร</button>
+
+                </center>
+                </div><!-- /.box-content -->
+                </div><!-- /.imagebox style4 -->
+
+                <div class="imagebox style4">
+                                <div class="box-image">
+                                    <span class="item-new1">ขาย</span>
+                                    <a href="#" title="">
+                                        <img src="images/slider/bg-product06.jpg" alt="">
+
+                                    </a>
+
+                                </div><!-- /.box-image -->
+                                <div class="box-content">
+                                    
+                                    <div class="product-name">
+                                        <p>รายละเอียด</p>
+                                    <a href="#" title="" > <h4>ปลาทอง</h4></a>
+                                        
+                                        <p>หมวดหมู่ : ปลาทอง</p>
+                                        <p>ร้าน : ลุงชัยเลี้ยงปลา</p>
+                                        <p>ลาดกระบัง กรุงเทพมหานคร</p>
+                                        <div class="price">
+                                                <span type="body-text" class="sale">฿</span><span class="sale">100 บาท</span>
+                                            </div>
+
+                                    </div>
+                                    <br>
+                            
+                <center><button class="button-chat"> <img src="images/icons/chat_white.png" alt="" width="20">  แชท</button>
+                <button class="button-call"> <img src="images/icons/phone_white.png" alt="" width="20"> โทร</button>
+
+                </center>
+                </div><!-- /.box-content -->
+                </div><!-- /.imagebox style4 -->
+
+
+                        
+
+
+
+                            
+
+                            
+
+                        
+
+                        
+                        </div><!-- /.owl-carousel-3 -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-imagebox style4 -->
 
             
 
