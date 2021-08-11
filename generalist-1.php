b6104d7e99c2fec4ec9c7adf589cee7ec68628c3
Phinyo <phinyo.it@gmail.com>
2021-08-11 20:12:15 +0700

    Create generalist-1.php

---
+++ generalist-1.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"><!--<![endif]-->
+    <head>
+        <!-- Basic Page Needs -->
+        <meta charset="UTF-8">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <title>ร้านค้าของฉัน | ตลาดอิเล็กทรอนิกส์ปลาแห่งสยาม</title>
+
+        <meta name="author" content="Fishsiam">
+        <meta name="keywords" content="แพลตฟอร์ม,ตลาดอิเล็กทรอนิกส์,ปลาแห่งสยาม,สยาม,ปลาสวยงาม , ปลากัด, ปลาไทย">
+        <meta name="description" content="แพลตฟอร์ม,ตลาดอิเล็กทรอนิกส์,ปลาแห่งสยาม,สยาม,ปลาสวยงาม , ปลากัด, ปลาไทย">
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <!-- Boostrap style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
+
+        <!-- Theme style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+
+        <!-- Reponsive -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
+
+        <link rel="shortcut icon" href="favicon/favicon.png">
+
+
+    </head>
+
+
+    <body class="header_sticky">
+        <div class="boxed">
+ 
+            <div class="overlay"></div>
+
+            <!-- Preloader -->
+            <div class="preloader">
+                <div class="clear-loading loading-effect-2">
+                    <span></span>
+                </div>
+            </div><!-- /.preloader -->
+
+            
+           
+            <?php include 'inc/header-top.php'; ?>
+
+
+
+           
+
+<section class="flat-breadcrumb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="breadcrumbs">
+                            <li class="trail-item">
+                                <a href="#" title="">หน้าหลัก</a>
+                                <span><img src="images/icons/arrow-right.png" alt=""></span>
+                            </li>
+                           
+                            <li class="trail-end">
+                                <a href="#" title="">ร้านค้าของฉัน</a>
+                            </li>
+                        </ul><!-- /.breacrumbs -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-breadcrumb -->
+
+         <div class="container">
+   
+                            <div class="col-md-12 col-lg-12">
+                      
+                                 
+                                  
+                                        <div class="jumbotron ">
+                                 
+                                                
+            
+       
+                
+                        <div class="row">
+                            <div class="col-md-1">
+                                <span class="avatar avatar-xxl bradius" style="background-image: url(assets/images/faces/male/7.jpg)"></span>
+                            </div>
+                        <div class="col-md-2">
+                                <h3 class="mb-1 ">สมชาย มีดวง</h3>
+                                <p class="mb-4 ">หมายเลขสมาชิก : 12381741 <br>
+                                <span class="tag tag-green">ผ่านมาตราฐาน GAP</span><br>
+                               </p>
+                        </div>
+                            <div class="col-md-4">
+                            <p class="mb-4 ">
+                                <img src="images/icons/storefront_black.png" alt="" width="20"> รายการสินค้า : 100<br>
+                                 <img src="images/icons/star_outline_black.png" alt="" width="20"> คะแนนร้านค้า : 4.9 (การให้คะแนนทั้งหมด 99 คน)<br>
+                                 <img src="images/icons/public_black.png" alt="" width="20"> เข้าร่วมเมื่อ : 2 ปี ที่ผ่านมา
+                            </p>
+                            </div>
+
+                            <div class="col-md-4">
+                            <p class="mb-4 ">
+                              
+
+                            </p>
+                            </div>
+                        </div>  
+                 
+        
+                         
+        
+                                       
+                                        </div>
+                                 
+                           
+                            </div>
+
+
+
+                     
+        </div>
+
+            
+          
+         
+
+
+                <div class="container">
+                <div class="row">
+
+                 <?php include 'inc/menu-setting.php'; ?>
+                
+
+                <div class="col-md-9">
+                      <div class="card">
+                   
+                    <div class="card-header">
+                        <h3 class="card-title">เพิ่มรายการสินค้าใหม่</h3>
+                    </div>
+                 
+
+                                        <div class="col-md-12 col-lg-12">
+
+                                            <section class="flat-row flat-accordion">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="accordion">
+                               <div class="accordion-toggle">
+                                <div class="toggle-title ">
+                                <!-- <div class="toggle-title active"> -->
+                                  <i class="fa fa-cube" data-toggle="tooltip" title="" data-original-title="fa fa-cube"></i> เพิ่มสินค้าใหม่
+                                </div>
+                                <div class="toggle-content">
+
+            <div class="col-md-12 col-lg-12">
+                             <form  method="post" >
+                                   
+                                    <div class="form-group">
+                                                    <label class="form-label">หัวข้อสินค้า</label>
+                                                    <input type="text" class="form-control" name="" placeholder="ชื่อสินค้า เช่น ปลาสีส้มตัวเล็ก">
+                                                    <p>ไม่ควรใส่อักขระพิเศษ เช่น & * # ^</p>
+                                    </div>
+
+
+                                            <div class="form-group ">
+                                            <label class="form-label">หมวดหมู่สินค้า</label>
+                                            <select class="form-control select2 custom-select" data-placeholder="Choose one">
+                                                <option label="เลือกประเภทปลา">
+                                                </option>
+                                                <option value="1">ปลากัด</option>
+                                                <option value="2">ปลาอโรวาน่า</option>
+                                                <option value="3">ปลาหางนกยูง & ปลาสอด</option>
+                                                <option value="4">ปลาปอมปาดัวร์</option>
+                                                <option value="5">ปลาหมอสี</option>
+                                            </select>
+                                        </div>
+
+                                        
+                                                <div class="form-group">
+                                                    <label class="form-label">ราคาสินค้า</label>
+                                                    <input type="text" class="form-control" name="" placeholder="ระบุราคาเต็มของสินค้า / ค่าบริการ">
+                                                </div>
+                                        
+
+                                       
+                                                <div class="form-group">
+                                                    <label class="form-label">รูปภาพสินค้า</label>
+                                                   <input type="file" class="dropify" data-height="180" />
+                                                </div>
+                                    
+
+                                       
+                                                <div class="form-group">
+                                                    <label class="form-label">รายละเอียดสินค้า</label>
+                                                   <textarea class="form-control" name="" rows="6" placeholder="ข้อมูลเพิ่มเติม เช่น ขนาด สี น้ำหนัก ของปลา"></textarea>
+                                                </div>
+                                   
+
+                                       
+                                            <div class="form-group ">
+                                            <label class="form-label">พื้นที่ของสินค้า (จังหวัด)</label>
+                                            <select class="form-control select2 custom-select" data-placeholder="เลือกจังหวัด">
+                                                <option label="เลือกจังหวัด">
+                                                </option>
+                                                <option value="1">กรุงเทพมานคร</option>
+                                            </select>
+                                        </div>
+                                    
+
+                            
+                                            <div class="form-group ">
+                                            <label class="form-label">พื้นที่ของสินค้า (เขต/อำเภอ)</label>
+                                            <select class="form-control select2 custom-select" data-placeholder="เลือกเขต/อำเภอ">
+                                                <option label="เลือกเขต/อำเภอ">
+                                                </option>
+                                                <option value="1">กรุงเทพมานคร</option>
+                                            </select>
+                                        </div>
+                                  
+
+                                      
+                                                <div class="form-group">
+                                                    <label class="form-label">เบอร์โทรติดต่อ</label>
+                                                    <input type="text" class="form-control" name="" placeholder="เบอร์โทรติดต่อ">
+                                                </div>
+                                   
+
+                            
+                                               <div class="form-group ">
+                                                    <div class="form-label">ต้องการ</div>
+                                                    <div class="custom-controls-stacked">
+                                                        <label class="custom-control custom-radio">
+                                                            <input type="radio" class="custom-control-input" name="example-radios" value="option1" checked>
+                                                            <span class="custom-control-label">ขาย</span>
+                                                        </label>
+                                                        <label class="custom-control custom-radio">
+                                                            <input type="radio" class="custom-control-input" name="example-radios" value="option2">
+                                                            <span class="custom-control-label">ประมูล</span>
+                                                        </label>
+                                                      
+                                                       
+                                                    </div>
+                                                </div>
+                                     
+
+                                        <div class="col-md-12 col-lg-12">
+                                                คลิกปุ่ม "บันทึก" เพื่อยอมรับ ข้อกำหนดและเงื่อนไขการลงประกาศ <br><br>
+
+                                                <button type="button" class="btn btn-info mb-1">บันทึก</button>
+                                        </div>
+
+
+
+                          
+                    </form>
+
+
+            </div> 
+
+
+                                </div>
+                            </div><!-- /.accordion-toggle -->
+                            <div class="accordion-toggle">
+                                <div class="toggle-title">
+                                   <i class="fa fa-gavel" data-toggle="tooltip" title="" data-original-title="fa fa-gavel"></i> เพิ่มสินค้าประมูล
+                                </div>
+                                <div class="toggle-content">
+                                   
+
+
+
+                                </div>
+                            </div><!-- /.accordion-toggle -->
+                        
+                         
+                           
+                          
+                           
+                        </div><!-- /.accordion -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-accordion -->
+
+                                               
+                                        </div>   
+
+                                        
+
+                                        
+                   
+
+
+
+                </div>
+                  </div>
+                </div>
+                </div>
+        
+
+
+    
+
+
+    
+
+          <!-- <div class="row">
+            <div class="col-sm-3" align="center">สวัสดร</div>
+             <div class="col-sm-3" align="center">สวัสดร</div>
+         <div class="col-sm-3" align="center">สวัสดร</div>
+          <div class="col-sm-3" align="center">สวัสดร</div>
+
+</div> -->
+
+
+            
+
+
+
+
+
+
+
+             <?php include 'inc/footer.php'; ?>
+        </div><!-- /.boxed -->
+
+        <!-- Javascript -->
+        <script type="text/javascript" src="javascript/jquery.min.js"></script>
+        <script type="text/javascript" src="javascript/tether.min.js"></script>
+        <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+        <script type="text/javascript" src="javascript/waypoints.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.circlechart.js"></script>
+        <script type="text/javascript" src="javascript/easing.js"></script>
+<script type="text/javascript" src="javascript/jquery.zoom.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+        <script type="text/javascript" src="javascript/owl.carousel.js"></script>
+        
+        <script type="text/javascript" src="javascript/jquery-ui.js"></script>
+        <script type="text/javascript" src="javascript/jquery.mCustomScrollbar.js"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtRmXKclfDp20TvfQnpgXSDPjut14x5wk&region=GB"></script>
+        <script type="text/javascript" src="javascript/gmap3.min.js"></script>
+        <script type="text/javascript" src="javascript/waves.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.countdown.js"></script>
+
+        <script type="text/javascript" src="javascript/main.js"></script>
+        <!---Tabs JS-->
+        <script src="assets/plugins/tabs/jquery.multipurpose_tabcontent.js"></script>
+
+    
+
+       <script>
+            $(function(e) {
+                $(".first_tab").champ();
+                $(".accordion_example").champ({
+                    plugin_type: "accordion",
+                    side: "left",
+                    active_tab: "3",
+                    controllers: "true"
+                });
+
+                $(".second_tab").champ({
+                    plugin_type: "tab",
+                    side: "right",
+                    active_tab: "1",
+                    controllers: "false"
+                });
+
+            });
+        </script>
+
+
+    </body> 
+</html>
+
+<!-- <script language=JavaScript>
+
+var message="ตลาดอิเล็กทรอนิกส์ปลาแห่งสยาม";
+
+function clickIE() {if (document.all) {alert(message);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {alert(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+document.oncontextmenu=new Function("return false")
+
+</script> -->
