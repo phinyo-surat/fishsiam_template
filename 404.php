ce193aa2d233fadf6753161898bd4a0911d634e7
Phinyo <phinyo.it@gmail.com>
2021-12-05 08:00:56 +0700

    Create 404.php
    update

---
+++ 404.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"><!--<![endif]-->
+    <head>
+        <!-- Basic Page Needs -->
+        <meta charset="UTF-8">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <title>เข้าสู่ระบบ | ตลาดอิเล็กทรอนิกส์ปลาแห่งสยาม</title>
+
+        <meta name="author" content="Fishsiam">
+        <meta name="keywords" content="แพลตฟอร์ม,ตลาดอิเล็กทรอนิกส์,ปลาแห่งสยาม,สยาม,ปลาสวยงาม , ปลากัด, ปลาไทย">
+        <meta name="description" content="แพลตฟอร์ม,ตลาดอิเล็กทรอนิกส์,ปลาแห่งสยาม,สยาม,ปลาสวยงาม , ปลากัด, ปลาไทย">
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <!-- Boostrap style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
+
+        <!-- Theme style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+
+        <!-- Reponsive -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
+
+        <link rel="shortcut icon" href="favicon/favicon.png">
+        <link href="stylesheets/dashboard.css" rel="stylesheet" />
+
+
+
+    </head>
+    <style>
+.button-chat {
+  background-color: #0080fe;
+ 
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.button-chat:hover {
+  background-color: orange;
+  color: #fff;
+}
+
+
+.button-call {
+  background-color: orange;
+
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.button-call:hover {
+  background-color: orange;
+  color: #fff;
+}
+
+.button-bid {
+  background-color: orange;
+
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.button-bid:hover {
+  background-color: orange;
+  color: #fff;
+}
+
+</style>
+
+
+    <body class="header_sticky">
+        <div class="boxed">
+ 
+            <div class="overlay"></div>
+
+            <!-- Preloader -->
+            <div class="preloader">
+                <div class="clear-loading loading-effect-2">
+                    <span></span>
+                </div>
+            </div><!-- /.preloader -->
+
+            
+ <?php include 'inc/header-top.php'; ?>
+
+
+
+           
+
+<section class="flat-breadcrumb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="breadcrumbs">
+                            <li class="trail-item">
+                                <a href="#" title="">หน้าหลัก</a>
+                                <span><img src="images/icons/arrow-right.png" alt=""></span>
+                            </li>
+                           
+                            <li class="trail-end">
+                                <a href="#" title="">404 NOT FOUND</a>
+                            </li>
+                        </ul><!-- /.breacrumbs -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-breadcrumb -->
+
+            
+            <section class="flat-error">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                    </div><!-- /.col-md-2 -->
+                    <div class="col-md-8">
+                        <div class="wrap-error center">
+                            <div class="header-error">
+                                <img src="images/banner_boxes/error.png" alt="">
+
+
+                                <!-- <h1>Sorry but we couldn’t find the page you are looking for.</h1>
+                                <p>Please check to make sure you’ve typed the URL correctly. Maybe try a search?</p> -->
+
+
+                                <h1>ขออภัย เราไม่พบหน้าที่คุณกำลังค้นหา</h1>
+                                <p>โปรดตรวจสอบให้แน่ใจว่าคุณได้พิมพ์ URL ถูกต้อง</p>
+
+                                
+                            </div><!-- /.header-error -->
+                            <div class="content-error">
+                                <div class="form-search-error">
+                                    <form action="#" method="get" accept-charset="utf-8">
+                                        <div class="search-input">
+                                            <input type="text" name="search" placeholder="Search">
+                                            <button type="submit">ค้นหา</button>
+                                        </div>
+                                    </form>
+                                </div><!-- /.form-search-error -->
+                            </div><!-- /.content-error -->
+                        </div><!-- /.wrap-error -->
+                    </div><!-- /.col-md-8 -->
+                    <div class="col-md-2">
+                    </div><!-- /.col-md-2 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.flat-error -->
+
+        
+
+
+
+
+
+            <footer class="background">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="widget-ft widget-about">
+                                <div class="logo logo-ft">
+                                    <a href="index.html" title="">
+                                        <img src="images/logos/Logo_th-1.png" alt="">
+                                    </a>
+                                </div><!-- /.lgo-ft -->
+                                <div class="widget-content">
+                                    
+                                    <div class="info">
+                                        
+                                        <p>ติดต่อฝ่ายบริการลูกค้า</p>
+                                        <p>โทร : (+66) 12345678 <br/>
+                                            จ-ศ 09:00 - 17:00 น.
+                                        </p>
+                                    </div>
+                                </div><!-- /.widget-content -->
+                            
+                            </div><!-- /.widget-about -->
+                        </div><!-- /.col-lg-3 col-md-6 -->
+                        <div class="col-lg-2 ">
+                            <div class="widget-ft widget-categories-ft">
+                                <div class="widget-title">
+                                    <h3>ช่วยเหลือ</h3>
+                                </div><!-- /.widget-title -->
+                                <ul class="cat-list-ft">
+                                    <li>
+                                        <a href="#" title="">วิธีการช้อปออนไลน์</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="">คำถามที่พบบ่อย</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="">ติดตามสินค้า</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="">ติดต่อเรา</a>
+                                    </li>
+                                    
+                                </ul><!-- /.cat-list-ft -->
+                            </div><!-- /.widget-categries-ft -->
+                        </div><!-- /.col-lg-3 col-md-6 -->
+                        <div class="col-lg-3 ">
+                            <div class="widget-ft widget-categories-ft">
+                                <div class="widget-title">
+                                    <h3>เกี่ยวกับเรา</h3>
+                                </div><!-- /.widget-title -->
+                                <ul class="cat-list-ft">
+                                    <li>
+                                        <a href="#" title="">เกี่ยวกับเรา</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="">นโยบายของเรา</a>
+                                    </li>
+                                    <li>
+                                        <a href="privacy-policy.html" title="">นโยบายความเป็นส่วนตัว</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="">ผู้ติดต่อออนไลน์</a>
+                                    </li>
+                                    
+                                </ul><!-- /.cat-list-ft -->
+                            </div><!-- /.widget-categries-ft -->
+                        </div><!-- /.col-lg-3 col-md-6 -->
+                        
+                        <div class="col-lg-2 ">
+                            <div class="widget-ft ">
+                                <div class="widget-title">
+                                    <h3>ติดตามพัสดุ</h3>
+                                </div><!-- /.widget-title -->
+                    <a href ="https://www.thailandpost.co.th" target="_blank"><img src="images/icons/thaiPos.png" alt="" width="90"></a>
+                    <a href ="https://th.kerryexpress.com" target="_blank"><img src="images/icons/kerry.png" alt="" width="90"></a>
+                    <a href ="https://www.scgexpress.co.th" target="_blank"><img src="images/icons/scg.png" alt="" width="90"></a>
+                                    
+                            </div><!-- /.widget-categries-ft -->
+                        </div><!-- /.col-lg-3 col-md-6 -->
+
+                        <div class="col-lg-2 ">
+                            <div class="widget-ft widget-categories-ft">
+                                <div class="widget-title">
+                                    <h3>ติดตามเรา</h3>
+                                </div><!-- /.widget-title -->
+                                <ul class="cat-list-ft">
+                                    <li>
+                                        <i class="fa fa-facebook" aria-hidden="true"> </i> Facebook
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-instagram" aria-hidden="true"></i> Instagram
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-twitter" aria-hidden="true"></i> Twitter 
+                                    </li>
+                                    
+                                    
+                                </ul><!-- /.cat-list-ft -->
+                            </div><!-- /.widget-categries-ft -->
+                        </div><!-- /.col-lg-3 col-md-6 -->
+                    </div><!-- /.row -->
+                    
+
+                </div><!-- /.container -->
+            </footer><!-- /footer -->
+
+                <section class="footer-bottom style1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p class="copyright">Copyright 2001-2021 © thefishsiam</p>
+                            <p class="btn-scroll">
+                                <a href="#" title="">
+                                    <img src="images/icons/top.png" alt="">
+                                </a>
+                            </p>
+                        </div><!-- /.col-md-12 -->
+                    </div><!-- /.row -->
+                </div><!-- /.container -->
+            </section><!-- /.footer-bottom -->
+
+        </div><!-- /.boxed -->
+
+        <!-- Javascript -->
+        <script type="text/javascript" src="javascript/jquery.min.js"></script>
+        <script type="text/javascript" src="javascript/tether.min.js"></script>
+        <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+        <script type="text/javascript" src="javascript/waypoints.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.circlechart.js"></script>
+        <script type="text/javascript" src="javascript/easing.js"></script>
+<script type="text/javascript" src="javascript/jquery.zoom.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+        <script type="text/javascript" src="javascript/owl.carousel.js"></script>
+        
+        <script type="text/javascript" src="javascript/jquery-ui.js"></script>
+        <script type="text/javascript" src="javascript/jquery.mCustomScrollbar.js"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtRmXKclfDp20TvfQnpgXSDPjut14x5wk&region=GB"></script>
+        <script type="text/javascript" src="javascript/gmap3.min.js"></script>
+        <script type="text/javascript" src="javascript/waves.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.countdown.js"></script>
+
+        <script type="text/javascript" src="javascript/main.js"></script>
+
+    
+
+        <script>
+          $(function () {
+
+    // INITIALIZE DATEPICKER PLUGIN
+    $('.datepicker').datepicker({
+        clearBtn: true,
+        format: "dd/mm/yyyy"
+    });
+
+
+    // FOR DEMO PURPOSE
+    $('#reservationDate').on('change', function () {
+        var pickedDate = $('input').val();
+        $('#pickedDate').html(pickedDate);
+        
+    });
+});
+        </script>
+
+
+    </body> 
+</html>
+
+<!-- <script language=JavaScript>
+
+var message="ตลาดอิเล็กทรอนิกส์ปลาแห่งสยาม";
+
+function clickIE() {if (document.all) {alert(message);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {alert(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+document.oncontextmenu=new Function("return false")
+
+</script> -->
+
+ <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+  
+     <img src="images/banner_boxes/banner-4.jpg" alt="">
+     <div class="row">
+     <div class="col-md-1">
+        </div>
+        <div class="col-md-10">
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">ชื่อผู้ใช้งาน :</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+         <div class="form-group">
+            <label for="recipient-name" class="col-form-label">รหัสผ่าน :</label>
+            <input type="password" class="form-control" id="recipient-name">
+          </div>
+          <center>หรือ <br>
+   
+            <div class="col-md-12">
+                                                <center>
+                                                    <button type="button" class="btn btn-facebook"><i class="fa fa-facebook mr-2"></i>เข้าสู่ระบบด้วย Facebook</button>
+                                                </center>
+                                                <br>
+
+                                               
+                                                
+                                            </div>
+          
+            
+
+      </center>
+
+
+        </form>
+      </div>
+      </div>
+     
+
+       <div class="col-md-1">
+        </div>
+
+        </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
+        <button type="button" class="btn btn-warning">เข้าสู่ระบบ</button>
+      </div>
+    </div>
+  </div>
+</div>
